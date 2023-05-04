656c2dd958f15c430d95b6397282920f9f1b17ed
RinnyVeronica <Rinnyveronica555@gmail.com>
2023-05-04 21:56:43 +0530

    FINAL CODE

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import FlashCardCreate from './Pages/FlashCardCreate';
 import Cards from './Pages/Cards';
 import AllCreatedCards from './Pages/AllCreatedCards';
 import Home from './Pages/Home';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route,Router } from 'react-router-dom'
 import { createContext, useState } from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
@@ -23,11 +23,13 @@ function App() {
             <div id={theme} className={`w-full bg-white font-Montserrat flex items-center space-x-10 mb-3`}>
                 <div className='px-5 xl:px-32 container mx-auto'>
                     <Home />
+                    
                     <Routes>
                         <Route path='/' element={<FlashCardCreate theme={theme} />} />
                         <Route path='/myflashcard' element={<Cards theme={theme} />} />
                         <Route path='/AllCreatedCards/:groupId' element={<AllCreatedCards theme={theme} />} />
                     </Routes>
+                   
                 </div>
                 <ToastContainer />
             </div>
